98c34f6efbc1c7e9a1d8e1466c62127e258b92f9
tuyetnhu06 <kimhuynhtuyetnhu0611@gmail.com>
2026-04-15 23:40:28 +0700

    find parent locator and reusing locator

--- tests/lesson26_childelement.spec.ts
+++ tests/lesson26_childelement.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -11,7 +11,7 @@ test.skip('example', async ({ page }) => {
     page.locator(':text("Using")').click()
 })
 
-test('locating child elements', async ({ page }) => {
+test.skip('locating child elements', async ({ page }) => {
     //use space
     await page.locator('nb-card nb-radio :text-is("Option 1")').click()
     await page.locator('nb-card').locator(':text-is("Option 1")').click()
@@ -19,4 +19,36 @@ test('locating child elements', async ({ page }) => {
     await page.locator('nb-card').getByRole('button', { name: "Sign in" }).first().click()
     //have 4 nb-card, nth(3) is the 4th card Tất cả nb-card  →  lấy cái thứ 4  →  tìm button bên trong  →  click
     await page.locator('nb-card').nth(3).getByRole('button').click()
+})
+
+test.skip('locating parent elements', async ({ page }) => {
+    //tro vao cai card. sau do tro vao email text box (getbyrole)
+    await page.locator('nb-card', { hasText: "Using the Grid" }).getByRole('textbox', { name: "Email" }).click()
+
+    await page.locator('nb-card', { has: page.locator('#inputEmail1') }).getByRole('textbox', { name: "Email" }).click()
+
+    await page.locator('nb-card').filter({ hasText: "Basic form" }).getByRole('textbox', { name: "Email" }).click()
+
+    await page.locator('nb-card').filter({ has: page.locator('[placeholder="Website"]') }).getByRole('button', { name: "SUBMIT" }).click()
+
+    await page.locator('nb-card').filter({ has: page.locator('.size-medium') }).getByPlaceholder('Message').click()
+
+    await page.locator('nb-card').filter({ has: page.locator('nb-checkbox') }).filter({ hasText: 'Sign in' }).getByRole('textbox', { name: "Email" }).click()
+
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', { name: "Email" }).click()
+})
+
+test('Reusing locator', async ({ page }) => {
+    //create constant de tranh duplication
+
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic form" })
+    const emailField = basicForm.getByRole('textbox', { name: "Email" })
+
+
+    await emailField.fill('nhukim@gmail.com')
+    await basicForm.getByRole('textbox', { name: "Password" }).fill('123456')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.getByRole('button', { name: "SUBMIT" }).click()
+
+    await expect(emailField).toHaveValue('nhukim@gmail.com')
 })
